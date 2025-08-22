2a0179e44d176e13012e277132a80ea0951eeaf9
Tyler Pearson <tyler.a.pear@gmail.com>
2025-08-21 20:35:31 -0500

    adds intro section

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css'
+
+export function HomePage() {
+  return (
+    <>
+      <div className="intro-container">
+        <h4>My name is</h4>
+        <h1>Tyler Pearson</h1>
+        <h4>And I'm an aspiring Software Engineer</h4>
+      </div>
+    </>
+  );
+}
