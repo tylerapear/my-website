73ea5b103aaea1defcd6ec90240950a10c74dbd6
Tyler Pearson <tyler.a.pear@gmail.com>
2025-08-21 22:36:56 -0500

    fixes typo on homepage

--- src/components/XeldaGame.jsx
+++ src/components/XeldaGame.jsx
@@ -5,10 +5,10 @@ export function XeldaGame() {
     <div className='xelda-game-component'>
       <h3>Check out my latest project...</h3>
       <h2>The Legend of 'Xelda'</h2>
-      <iframe src='/public/game.html'></iframe>
+      <iframe src='/game.html'></iframe>
       <h5>'W', 'A', 'S', and 'D' to move; 'J' to attack</h5>
       <p>
-        Well, the game is a demonstration of my project anyway. I'm developing a 2D game engine Python and Pygame.
+        Well, the game is a demonstration of my project anyway. I'm developing a 2D game engine using Python and Pygame.
         Once this project is complete, developers will be able to use it to develop any 2D Zelda-like game they can imagine.
       </p>
     </div>
