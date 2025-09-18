920098d3037306709922a0b93f1e00c966be554d
Tyler Pearson <tyler.a.pear@gmail.com>
2025-09-17 23:00:12 -0500

    Updates NESY-Engine details on homepage

--- src/components/XeldaGame.jsx
+++ src/components/XeldaGame.jsx
@@ -4,12 +4,19 @@ export function XeldaGame() {
   return(
     <div className='xelda-game-component'>
       <h3>Check out my latest project...</h3>
-      <h2>The Legend of 'Xelda'</h2>
+      <h2>NESY-Engine</h2>
       <iframe src='/game.html'></iframe>
       <h5>'W', 'A', 'S', and 'D' to move; 'J' to attack</h5>
       <p>
-        Well, the game is a demonstration of my project anyway. I'm developing a 2D game engine using Python and Pygame.
-        Once this project is complete, developers will be able to use it to develop any 2D Zelda-like game they can imagine.
+        I'm developing an open source 2D game engine package using Python and Pygame. The game you see here is a demo to display the engine's capabilities.
+        Once this project is complete, developers will be able to use it to develop any 2D NES-style game they can imagine. 
+        Check out the source code at <a href="https://github.com/tylerapear/NESY-Engine">https://github.com/tylerapear/NESY-Engine</a>
+      </p>
+      <h5>Huge thanks to all NESY-Engine Contributors!</h5>
+      <p class = "centered-paragraph">
+        <a href="https://github.com/tylerapear">tylerapear</a>
+        <br></br>
+        <a href="https://github.com/punz321">punz321</a>
       </p>
     </div>
   );
