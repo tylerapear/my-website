e5aff801d8261ff8afde4cc48f5c4a4daaab25d7
Tyler Pearson <tyler.a.pear@gmail.com>
2025-09-20 23:18:10 -0500

    fixes image refs by placing them in /public

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ export function Footer() {
     <footer>
       <div class="footer-wrapper">
         <a href="https://github.com/tylerapear" target="_blank">
-          <img class="img-social" src="src/assets/github-mark-white.png"></img>
+          <img class="img-social" src="github-mark-white.png"></img>
         </a>
         <a href="https://www.linkedin.com/in/tyler-a-pearson" target="_blank">
-          <img class="img-social" src="src/assets/InBug-White.png"></img>
+          <img class="img-social" src="InBug-White.png"></img>
         </a>
         <a href="https://www.linkedin.com/in/tyler-a-pearson" target="_blank">
-          <img class="img-social" src="src/assets/x_logo-white.png"></img>
+          <img class="img-social" src="x_logo-white.png"></img>
         </a>
       </div>
     </footer>
